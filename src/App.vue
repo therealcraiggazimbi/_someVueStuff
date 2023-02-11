6ff462980550fa936eb8078307a680f967181981
therealcraiggazimbi <craiggazimbi@gmail.com>
2023-02-11 02:38:39 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <input type="text" ref="name" />
+    <button @click="handleClick">click me</button>
+    <div v-if="showModal">
+      <Modal theme="sale" @close="toggleModal">
+        <template v-slot:links>
+          <a href="#">Sign up now </a>
+          <a href="#">more info </a>
+        </template>
+        <h1>Hello</h1>
+        <p>Me me</p>
+      </Modal>
+    </div>
+
+    <button @click="toggleModal">open modal</button>
+  </div>
+</template>
+
+<script>
+import Modal from "./components/Modal";
+
+export default {
+  components: { Modal },
+  name: "App",
+  data() {
+    return {
+      title: "My first Vue App : )",
+      showModal: false,
+    };
+  },
+
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add("active");
+      this.$refs.name.focus();
+    },
+
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+  },
+
+  component: {
+    Modal,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  text-decoration: underline;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <slot></slot>
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["theme"],
+
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+.modal.sale {
+  background: red;
+  color: white;
+}
+</style>
